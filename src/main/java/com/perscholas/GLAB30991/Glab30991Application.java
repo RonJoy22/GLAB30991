9173bfc2278348d111729f3f42307853da6153ed
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-09-10 16:30:36 -0400

    Initial commit

---
+++ src/main/java/com/perscholas/GLAB30991/Glab30991Application.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB30991;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30991Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30991Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/GLAB30991/Glab30991ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB30991;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30991ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
